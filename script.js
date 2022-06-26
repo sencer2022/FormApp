b0315ccc2bac3645873539039bea191d433fd360
sencer2022 <senceralpdogdu@gmail.com>
2022-06-26 15:28:55 +0300

    validateEmail added

--- script.js
+++ script.js
@@ -5,42 +5,53 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const repassword = document.getElementById('repassword');
 
-function error(input){
+function error(input, message){
     input.className = 'form-control is-invalid';
+    const div = input.nextElementSibling;
+    div.innerText = message;
+    div.className = 'invalid-feedback'
 };
 
 function success(input){
    input.className = 'form-control is-valid';
 };
 
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+};
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
     if(username.value ===''){
-        error(username);
+        error(username, "Type username please..");
     }else {
         success(username);
     }
 
     if(email.value ===''){
-        error(email);
+        error(email, "Type E-mail please..");
+    }else if(!validateEmail(email.value)){
+        error(email, "Type the E-mail correctly please.. ")
     }else {
         success(email);
     }
 
     if(password.value ===''){
-        error(password);
+        error(password, "Type Password please..");
     }else {
         success(password);
     }
 
     if(repassword.value ===''){
-        error(repassword);
+        error(repassword, "Type Repassword please..");
     }else {
         success(repassword);
     }
 
     if(username.value ==='' || email.value ==='' || password.value ==='' || repassword.value ===''){
+        card.className = 'card error';
+    }else if(!validateEmail(email.value)){
         card.className = 'card error';
     }else{
         card.className = 'card success';
