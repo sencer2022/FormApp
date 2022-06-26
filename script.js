d9b8c8b3b001a5a7d8903104f2f84e746c1d7f68
sencer2022 <senceralpdogdu@gmail.com>
2022-06-26 17:36:59 +0300

    code fixed

--- script.js
+++ script.js
@@ -5,51 +5,53 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const repassword = document.getElementById('repassword');
 
-function error(input1, message){
-    input1.className = 'form-control is-invalid';
-    const div = input1.nextElementSibling;
+function error(input, message){
+    input.className = 'form-control is-invalid';
+    const div = input.nextElementSibling;
     div.innerText = message;
     div.className = 'invalid-feedback'
 };
 
-function success(input1){
-   input1.className = 'form-control is-valid';
+function success(input){
+   input.className = 'form-control is-valid';
 };
 
-function checkEmail(input1) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
-    if(re.test(String(input1.value).toLowerCase())){
-        success(input1);
+    if(re.test(String(input.value).toLowerCase())){
+        success(input);
     }else{
         error(email, 'Type the E-mail correctly please..');
+        //card.className = 'card error';
     }
 
-    //return re.test(input1.value);
-    return re.test(String(input1.value).toLowerCase());
+    //return re.test(input.value);
+    return re.test(String(input.value).toLowerCase());
 };
 
-function checkRequirement(input1s){
+function checkRequirement(inputs){
    
-    input1s.forEach(function(input1){
-        if(input1.value===''){
-            error(input1,`Type the ${input1.id} please..`);
+    inputs.forEach(function(input){
+        if(input.value===''){
+            error(input,`Type the ${input.id} please..`);
+            //card.className = 'card error';
         }else{
-            success(input1);
+            success(input);
         };
     });
 
 };
 
-function checkLength(input1, min, max){
-    if(input1.value.length < min){
-        error(input1, `${input1.id} has to be min ${min} characters.`);
+function checkLength(input, min, max){
+    if(input.value.length < min){
+        error(input, `${input.id} has to be min ${min} characters.`);
         card.className = 'card error';
-    }else if(input1.value.length > max){
-        error(input1, `${input1.id} has to be max ${max} characters.`);
+    }else if(input.value.length > max){
+        error(input, `${input.id} has to be max ${max} characters.`);
         card.className = 'card error';
     }else{
-        success(input1)
+        success(input)
     }
 }
 
