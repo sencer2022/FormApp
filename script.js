a6f6ef80693cb4ad7bdfd8fb613c98c1124f8880
sencer2022 <senceralpdogdu@gmail.com>
2022-06-26 13:29:46 +0300

    error control with classList / className

--- script.js
+++ script.js
@@ -5,32 +5,34 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const repassword = document.getElementById('repassword');
 
+
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
     //console.log(username.value);
     //console.log(password.value);
 
     if(username.value ===''){
-        card.classList ='card error1';
+        card.className ='card error1';
         //username.className = 'form-control error1';
         username.classList = 'form-control is-invalid';
     }else {
-        username.classList = 'form-control is-valid';
+        username.className = 'form-control is-valid';
     }
 
     if(email.value ===''){
-        card.classList ='card error1';
-        email.classList = 'form-control is-invalid';
+        card.className ='card error1';
+        email.className = 'form-control is-invalid';
     }
 
     if(password.value ===''){
-        card.classList ='card error1';
-        password.classList = 'form-control is-invalid';
+        card.className ='card error1';
+        password.className = 'form-control is-invalid';
     }
 
     if(repassword.value ===''){
-        card.classList ='card error1';
-        repassword.classList = 'form-control is-invalid';
+        card.className ='card error1';
+        repassword.className = 'form-control is-invalid';
     }
 });
 
